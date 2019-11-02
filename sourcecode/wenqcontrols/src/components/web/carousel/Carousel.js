ed2765a866ad2e28be640dcc138396af5d3f18c7
wenq <wen49880132@163.com>
2019-11-02 17:12:16 +0800

    引入第三方库immutable

--- sourcecode/wenqcontrols/src/components/web/carousel/Carousel.js
+++ sourcecode/wenqcontrols/src/components/web/carousel/Carousel.js
@@ -4,26 +4,48 @@
  * @Author: wenq
  * @Date: 2019-11-02 15:26:57
  * @LastEditors: wenq
- * @LastEditTime: 2019-11-02 16:41:42
+ * @LastEditTime: 2019-11-02 17:09:00
  */
 import React, { Component } from 'react'
 import * as styles from './Carousel.css'
 import classnames from 'classnames'
 
 export default class Carousel extends Component {
-    constructor(props) {
-        super(props)
-    } 
 
     render() {
-        // let { style={}, className='', ...others }; //es6语法才支持解构
         let { style, className, ...others } = this.props;
-        className = classnames(styles['carousel'], className);
-        return <div style={style} className={className} {...others}>Carousel</div>
+        className = classnames('carousel', className);
+        //组件标识信息
+        let data_custom = {
+            type: 'Carousel'
+        };
+        data_custom = JSON.stringify(data_custom);
+        return <div data-data_custom={data_custom} style={style} className={className} {...others}>
+            <p>Carousel</p>
+            <CarouselFoatMenu className={'carousel-floatmenu'} />
+        </div>
     }
 }
 
 Carousel.defaultProps = {
     style: {},
     className: ''
+}
+
+//轮播图底部切换按钮组件
+class CarouselFoatMenu extends Component {
+
+    render() {
+        let { style, className, items, ...others } = this.props;
+        if (!items || items.size < 1) {
+            return null;
+        }
+        return <ul style={style} className={classnames('carouselfloatmenu', className)}>
+            {
+                items && items.map((item, index) => {
+                    return <li className={'floatitem'}>{index}</li>
+                })
+            }
+        </ul>
+    }
 }
