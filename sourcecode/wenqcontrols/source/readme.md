092a1c9ff5db68347b94a3fdfa102822a506233d
wenq <wen49880132@163.com>
2019-11-03 18:58:05 +0800

    增加测试用图片文件

---
+++ sourcecode/wenqcontrols/source/readme.md
@@ -0,0 +1,9 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: wenq
+ * @Date: 2019-11-03 18:24:58
+ * @LastEditors: wenq
+ * @LastEditTime: 2019-11-03 18:25:21
+ -->
+### 统一资源目录，比如图片等
--- sourcecode/wenqcontrols/src/components/web/carousel/Carousel.js
+++ sourcecode/wenqcontrols/src/components/web/carousel/Carousel.js
@@ -4,16 +4,27 @@
  * @Author: wenq
  * @Date: 2019-11-02 15:26:57
  * @LastEditors: wenq
- * @LastEditTime: 2019-11-02 17:09:00
+ * @LastEditTime: 2019-11-03 18:22:01
  */
 import React, { Component } from 'react'
 import * as styles from './Carousel.css'
 import classnames from 'classnames'
+import {Map, List, fromJS} from 'immutable'
+
+//控件数据
+let test_data = [
+    {id: 1, content: 'xxx1.png'},
+    {id: 2, content: 'xxx2.png'},
+    {id: 3, content: 'xxx3.png'},
+    {id: 4, content: 'xxx4.png'},
+    {id: 5, content: 'xxx5.png'},
+]
+test_data = fromJS(test_data)
 
 export default class Carousel extends Component {
 
     render() {
-        let { style, className, ...others } = this.props;
+        let { style, className, data, ...others } = this.props;
         className = classnames('carousel', className);
         //组件标识信息
         let data_custom = {
